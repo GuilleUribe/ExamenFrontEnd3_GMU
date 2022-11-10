2aaa5af86721a13f6c4f3a8242adaca854f87fa7
GuilleUribe <guille0309@gmail.com>
2022-11-10 17:41:25 -0500

    subiendo la entrega del primer parcial de Front End III por Guillermo Mejía Uribe

---
+++ evaluacion-noviembre-22/src/App.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import Estatus from './components/Estatus';
+import Posteos from './components/Posteos';
+// El componente App es el padre de:
+// - Estatus
+// - Posteos
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de likes.
+// MÉTODOS: App debe tener un método para incrementar este número y que pueda ser ejecutado por su nieto Post.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos para que manipulen o lean su estado.
+
+function App() {
+  const [total, setTotal] = useState(0)
+
+  const incrementar = () => {
+    setTotal(total +1)
+  }
+  return (
+    <div className="App">
+      <Estatus total={total} />
+      <Posteos incrementar={incrementar}/>
+    </div>
+  );
+}
+
+export default App;
